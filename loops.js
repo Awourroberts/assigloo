866a39f4a879392b4fcefe76040c544c2539b3d9
Awourrobberrt <sophierobberrt@gmail.com>
2021-04-04 20:05:18 +0300

    assignloo

---
+++ loops.js
@@ -0,0 +1,24 @@
+ const object={
+    a:20,
+   r:10,
+   p:15,
+   l:40,
+   w:90,
+   g:100,
+       
+
+    
+ }
+ for(const key in object){
+    console.log(object[key])
+ }
+
+
+
+let fruits=["banana","orange","kiwi","passion","quava"];
+let txt="";
+for(let x of fruits){
+    txt+=x;
+
+}
+console.log(fruits);
